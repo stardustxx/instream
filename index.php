f7ed5be8af9b03adcd7893923270a773c785981a
Eric Lee <e811226@gmail.com>
2014-10-25 13:47:26 -0700

    ..

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<script src="js/masonry.js" type="text/javascript"></script>
 	<script src="js/app.js" type="text/javascript"></script>
 	<script src="components/webcomponentsjs/webcomponents.js" type="text/javascript"></script>
-    <!--<script src="components/platform/platform.js" type="text/javascript"></script>-->
+    <script src="components/polymer/polymer.js" type="text/javascript"></script>
 	<link rel="stylesheet" href="css/app.css">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="import" href="components/paper-button/paper-button.html">
