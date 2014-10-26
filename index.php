cd6aaa0bfe2f14df7cda4241e8060e037b0c034f
Eric Lee <e811226@gmail.com>
2014-10-25 17:24:06 -0700

    somwhow it works beautifully

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 	<meta charset="utf-5">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src="js/jquery.js" type="text/javascript"></script>
-	<script src="js/masonry.js" type="text/javascript"></script>
 	<script src="js/app.js" type="text/javascript"></script>
 	<script src="components/webcomponentsjs/webcomponents.js" type="text/javascript"></script>
 	<link rel="stylesheet" href="css/app.css">
-	<link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="import" href="components/paper-button/paper-button.html">
+    <link rel="import" href="components/polymer/polymer.html">
 	<link rel="import" href="components/core-toolbar/core-toolbar.html">
 	<link rel="import" href="components/core-icon/core-icon.html">
 	<link rel="import" href="components/core-icon-button/core-icon-button.html">
@@ -18,18 +18,18 @@
 	<link rel="import" href="components/paper-input/paper-input.html">
 	<link rel="import" href="components/paper-toast/paper-toast.html">
     <link rel="import" href="components/paper-shadow/paper-shadow.html">
-    <link rel="import" href="components/paper-elements/paper-elements.html">
     <link rel="import" href="components/paper-ripple/paper-ripple.html">
     <link rel="import" href="components/core-header-panel/core-header-panel.html">
 </head>
 <body>
     <core-header-panel mode = "seamed">
-        <core-toolbar id = "mainHeader">
-            <span flex>InStream</span>
+        <core-toolbar class = "mainHeader">
+            <span flex id = "logo">InStream</span>
             <paper-icon-button raised icon="refresh" onclick="updatepage()"></paper-icon-button>
             <paper-input id = "search" label = "Enter a search tag" value = "instadaily"></paper-input>
         </core-toolbar>
-    
+
+        
 	
         <div id = "beaker" class = "tbCenter">
             <!-- <section id="sform">
